a8f5e57348e1ef0b78aad15e70447f0121f90c06
Helter Pinheiro <helterpinheiro@gmail.com>
2022-01-26 02:50:46 -0300

    Use Cases

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,14 @@
-import { Router } from "express";
+import { response, Router } from "express";
+import multer from "multer"
+
 import { createCategoryController } from "../modules/Cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/Cars/useCases/listCategory";
+import { importCategoryController } from "../modules/Cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
+
+//configurações multer
+const upload = multer({dest: "./tmp"})
 
 categoriesRoutes.post("/", (request, response) => {
  return createCategoryController.handle(request, response);
@@ -10,6 +16,10 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response)
+})
+
+categoriesRoutes.post("/import", upload.single("file"),(request, response) => {
+  return importCategoryController.handle(request, response)
 })
 
 export { categoriesRoutes };
